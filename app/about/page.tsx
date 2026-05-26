710858d036277ea78e7ce27b08b2606aa6383ab9
indra.rakha <indra.abdul@dansmultipro.com>
2026-05-26 23:17:17 +0700

    [page] - Add page about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,39 @@
 export default function About() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">About</h1>
-      <p className="mt-4 text-lg">This is the About page.</p>
-    </div>
+    <main className="content-wrapper">
+      <div className="">
+        <div className="text-cmd">
+          <span>$</span>
+          <span>cat about.md</span>
+        </div>
+        <div className="mt-2">
+          If you could turn back time and be a child, would your dreams come
+          true?.
+          <br />
+          My childhood aspiration was to become an architect—designing towering,
+          futuristic, and magnificent buildings that would leave a lasting
+          impression on visitors. However, upon entering high school, I made the
+          decision to set that dream aside. I opted to be pragmatic, adapting to
+          market demands in order to secure my livelihood.
+          <br />
+          <br />
+          That was my circumstance in the past. How does this compare to the
+          youth of Kampung DAO? A 2017 LinkedIn survey revealed that only 13% of
+          professionals pursue careers that align with their childhood
+          aspirations. Furthermore, this data solely represents LinkedIn users;
+          the unrepresented demographic is significantly larger.
+          <br />
+          <br />
+          <b className="text-accent">So the question is:</b>
+          <br />
+          <div className="question-desc">
+            why are you here? Are you here to help, to teach the kids of Kampung
+            DAO?
+            <div className="blink" />
+          </div>
+          <br />
+        </div>
+      </div>
+    </main>
   );
 }
