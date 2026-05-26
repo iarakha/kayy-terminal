41914110a030ed9a2c29236beaa86e8fb6490f7f
indra.rakha <indra.abdul@dansmultipro.com>
2026-05-27 02:31:06 +0700

    [page] - Experience

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,8 +1,20 @@
 export default function Experience() {
   return (
-    <main style={{ padding: "20px" }}>
-      <h1>Experience</h1>
-      <p>This is the experience page.</p>
+    <main className="content-wrapper">
+      <h1 className="text-2xl font-bold">Experience is the best teacher</h1>
+      <div className="">
+        Kita menghabiskan sangat banyak waktu berharap kehidupan kita berbeda,
+        membanding-bandingkan diri kita dengan orang lain dan pada versi-versi
+        lain dari diri kita sendiri, padahal sebetulnya sebagian besar kehidupan
+        memiliki berbagai tingkatan baik dan buruk.
+        <br />
+        <br />
+        <div className="question-desc">
+          Ketika kau tinggal terlalu lama di satu tempat, kau lupa betapa luas
+          membentang dunia ini sebetulnya.
+          <div className="blink" />
+        </div>
+      </div>
     </main>
   );
 }
